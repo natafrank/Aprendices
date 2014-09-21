fb1a634c936e1469612dc1f52c9ac3929a7261d6
MigueLopez <miguelfrost78@gmail.com>
2014-09-20 22:15:48 -0500

    Adición de campos Email y Tel a User

--- View/ShowUser.php
+++ View/ShowUser.php
@@ -3,7 +3,9 @@
 	" <br/>Nombre: ",$this->model->name,
 	" <br/>Login: " ,$this->model->login,
 	" <br/>Pass: "  ,$this->model->pass,
-	" <br/>Tipo: "  ,$this->model->type;
+	" <br/>Tipo: "  ,$this->model->type,
+	" <br/>Email: " ,$this->model->email,
+	" <br/>Tel: "   ,$this->model->tel;
 ?>
 
 
--- View/UpdateUserShow.php
+++ View/UpdateUserShow.php
@@ -4,6 +4,8 @@
 		 '<br/>       Name    : ', $this -> model -> name,
 		 '<br/>       Login   : ', $this -> model -> login,
 		 '<br/>       Password: ', $this -> model -> pass,
-		 '<br/>       Type    : ', $this -> model -> type;
+		 '<br/>       Type    : ', $this -> model -> type,
+		 '<br/>       Email   : ', $this -> model -> email,
+		 '<br/>       Tel     : ', $this -> model -> tel;
 
 ?>
